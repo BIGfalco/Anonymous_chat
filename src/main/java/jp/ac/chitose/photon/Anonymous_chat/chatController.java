17837c310eb12a25d551b34c1b49f1e7832de263
b2170820 <b2170820@photon.chitose.ac.jp>
2019-12-11 11:40:43 +0900

    メニューの文字変えた

--- src/main/java/jp/ac/chitose/photon/Anonymous_chat/chatController.java
+++ src/main/java/jp/ac/chitose/photon/Anonymous_chat/chatController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class chatController {
+public class
+chatController {
 
     @Autowired
     private RoomService roomService;
