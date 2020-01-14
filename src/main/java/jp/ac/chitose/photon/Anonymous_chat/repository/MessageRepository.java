3bb76285514cd6f063242253d5709cb7242ca373
b2171470 <b2171470@photon.chitose.ac.jp>
2020-01-14 12:58:03 +0900

    リポジトリ追加

--- src/main/java/jp/ac/chitose/photon/Anonymous_chat/repository/MessageRepository.java
+++ src/main/java/jp/ac/chitose/photon/Anonymous_chat/repository/MessageRepository.java
@@ -1,4 +1,20 @@
 package jp.ac.chitose.photon.Anonymous_chat.repository;
 
+import jp.ac.chitose.photon.Anonymous_chat.form.Message;
+import jp.ac.chitose.photon.Anonymous_chat.form.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MessageRepository {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+    
+    public int insert(Message message, Room room,String userId) {
+        var sql = "insert into MESSAGE values(?,?,?,?)";
+        var n = jdbc.update(sql,room.getRoomId(),userId,message.getMessage(),message.getTimeStamp());
+        return n;
+    }
 }
